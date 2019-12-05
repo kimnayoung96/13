a8833161a0d0c94fa8239e43df5b99677b5885c9
kimnayoung96 <55428158+kimnayoung96@users.noreply.github.com>
2019-12-05 15:45:45 +0900

    14-2

--- keywords.c
+++ keywords.c
@@ -1,11 +1,11 @@
 #define MAX_NAME 30
-
+#define N_KEYS  14
 struct keytab {
 	int nums; //몇번 나오냐
 	char name[MAX_NAME]; 
 };
 
-struct keytab keywrds[13] = {
+struct keytab keywrds[14] = {
 	
 	{0,"int"},
 	{0,"float"},
@@ -19,7 +19,8 @@ struct keytab keywrds[13] = {
 	{0,"void"},
 	{0,"default"},
 	{0,"char"},
-	{0,"do"}
+	{0,"do"},
+	{0,"scanf"}  //0라는 것은 처음 값(숫자를 세는..)  
 	
 	
 };
@@ -30,7 +31,7 @@ void count_word(char *word) //내가 세려고 하는 거랑 입력된 거랑 같으면 늘림  (동
 
 
 	//각 keyword별로  
-	for (i = 0; i< 13; i++) {
+	for (i = 0; i<  N_KEYS; i++) {
 	
 		//if word랑  i번째 keyword랑 같으면 
 		if(strncmp(word, keywrds[i].name, strlen(keywrds[i].name))==0)
@@ -49,7 +50,7 @@ void print_word()
 {
 	int i;
 	//i번째 keyword에 대해서
-	for( i= 0; i < 13 ; i++)
+	for( i= 0; i < N_KEYS ; i++)
 		//printf(키워드 이름: 빈도수);
 		printf("%s : %d\n", keywrds[i].name, keywrds[i].nums);	 
 } 
